723d9c2c689ef53ca6afe3ad793cb137bbe026c2
Shubham-112 <shubham.dwc@gmail.com>
2019-09-25 11:32:11 +0530

    main file added

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from Addition import Addition
+from Multiply import Multiply
+from Division import Division
+from Subtract import Subtract
+
+print("Enter the operation to perform")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Division")
+print("4. Multiplication")
+
+inp = input()
